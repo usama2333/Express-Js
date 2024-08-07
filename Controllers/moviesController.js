0c132532396be1b8dba2da11433bb5e55bbf2175
usama2333 <usama.wizz@gmail.com>
2024-08-08 01:49:09 +0500

    delete movie

--- Controllers/moviesController.js
+++ Controllers/moviesController.js
@@ -68,7 +68,6 @@ exports.createMovie = async (req, res) => {
             status: 'failed',
             message: err.message
         })
-
     }
 
    
@@ -99,6 +98,21 @@ exports.createMovie = async (req, res) => {
 
 }
 
-exports.deleteMovie = (req, res) => {
+exports.deleteMovie = async (req, res) => {
+    try{
+        await Movie.findByIdAndDelete(req.params.id)
+    res.status(204).json({
+        status: 'success',
+        data: null
+    })
+
+    }catch(err){
+        res.status(404).json({
+            status: 'failed',
+            message: err.message
+        })
+    }
+
+    
    
 }
